fd4d7cdd5d9d618a8dca59e35b9410ab9adf077d
Lino Galiana <lino.galiana@insee.fr>
2020-01-22 11:41:24 +0100

    listcoeff method

---
+++ R/listcoeff.R
@@ -0,0 +1,42 @@
+
+#' A small method to extract coefficient names from
+#'  econometric models
+#'
+#' @param object An object
+#' @param ... Additional arguments
+#' @return A character vector with names
+#'  that can be passed to latex
+#' @export
+listcoeff <- function(object, ...){
+  UseMethod("listcoeff")
+}
+
+#' @rdname listcoeff
+listcoeff.light.zeroinfl <- function(object, ...){
+
+  if (!inherits(object, "light.zeroinfl")) stop("Object is not light.zeroinfl")
+
+  allcoeffs <- object$coefficients
+
+  allcoeffs <- c(rownames(allcoeffs$count),
+                 rownames(allcoeffs$zero))
+  allcoeffs <- unique(allcoeffs)
+
+  allcoeffs <- gsub(pattern = "\\_", replacement = "\\\\_", allcoeffs)
+
+  return(allcoeffs)
+}
+
+listcoeff.light.glm <- function(object, ...){
+
+  if (!inherits(object, "light.glm")) stop("Object is not light.glm")
+
+  allcoeffs <- object$coefficients
+
+  allcoeffs <- rownames(allcoeffs)
+  allcoeffs <- unique(allcoeffs)
+
+  allcoeffs <- gsub(pattern = "\\_", replacement = "\\\\_", allcoeffs)
+
+  return(allcoeffs)
+}
