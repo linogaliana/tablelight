fff90bdfa41c9a77377468e67b6f96254e044875
Lino Galiana <lino.galiana@insee.fr>
2020-01-23 17:56:20 +0100

    Create light_table method for list

--- tests/testthat/test-light_table.R
+++ tests/testthat/test-light_table.R
@@ -15,12 +15,25 @@ oglm <- oglmx::oglmx(
   data = iris
 )
 
-
-testthat::expect_equal(
-  class(texlight::light_table(oglm)),
-  "character"
+testthat::expect_warning(
+  x <- texlight::light_table(model = oglm,
+                             modeltype = "outcome"),
+  "attributes are not identical across measure variables"
 )
 
 
-texlight::light_table(model = list(oglm, oglm))
+testthat::expect_equal(
+  class(x),
+  "character"
+)
 
+testthat::expect_warning(
+  x2 <- texlight::light_table(model = list(oglm, oglm),
+                        modeltype = c("outcome","outcome")),
+  "attributes are not identical across measure variables"
+)
+
+testthat::expect_equal(
+  class(x2),
+  "character"
+)
