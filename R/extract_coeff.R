e8664b36ba02127506954c820f5dd51736806589
Lino Galiana <lino.galiana@insee.fr>
2020-01-29 18:20:18 +0100

    extract_coeff to light.zeroinfl

--- R/extract_coeff.R
+++ R/extract_coeff.R
@@ -59,6 +59,12 @@ extract_coeff <- function(object, ...){
 #' @export
 extract_coeff.default <- function(object, ...){
 
+  # if (inherits(object, "light.zeroinfl")){
+  #   return(
+  #     extract_coeff.light(object, ...)
+  #   )
+  # }
+
   coeff_list <- secoeff(object)
 
   tstat_var <- "Pr(>|t|)"
@@ -109,5 +115,40 @@ extract_coeff.oglmx <- function(object, ...){
                                   fixed = TRUE)
 
   return(text_coeff)
+}
+
+#' @rdname extract_coeff
+#' @export
+extract_coeff.light.zeroinfl <- function(object, ...){
+
+  args <- list(...)
+
+  coeff_list <- secoeff.light.zeroinfl(object, ...)
+
+  namescol <- colnames(coeff_list)
+  coeff_list <- data.frame(coeff_list)
+  colnames(coeff_list) <- namescol
+
+  tstat_var <- "Pr(>|z|)"
+  sd_var <- "Std. Error"
+
+  text_coeff <- paste0(format(coeff_list[,'Estimate'],digits = 3L,
+                              nsmall = 3L, big.mark=",", scientific = FALSE),
+                       sapply(coeff_list[,tstat_var], signif_stars)
+  )
+  text_coeff <- gsub(x = text_coeff, pattern = " ", replacement = "")
+
+  text_sd <- paste0("(",format(coeff_list[,sd_var],
+                               digits = 3L,
+                               nsmall = 3L, big.mark=",", scientific = FALSE),
+                    ")")
+  text_sd <- gsub(x = text_sd, pattern = " ", replacement = "")
+
+  text_coeff <- cbind("variable" = rownames(coeff_list),text_coeff, text_sd)
+  text_coeff[,'variable'] <- gsub("_","\\_",text_coeff[,'variable'],
+                                  fixed = TRUE)
+
+  return(text_coeff)
+
 }
 
