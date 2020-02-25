f9171b3db929441194dfb3b3ff773c56b8a47d3e
Lino Galiana <lino.galiana@insee.fr>
2020-02-25 16:47:15 +0100

    BIC method

---
+++ R/BIC.R
@@ -0,0 +1,37 @@
+#' Bayesian information criterion implementation for stripped objects
+#'
+#' @param object A fitted model object for which there exists a logLik
+#'  method to extract the corresponding log-likelihood, or
+#'  an object inheriting from class logLik.
+#' @param ... Optionally more fitted model objects
+#'
+#'
+#' Formula \eqn{-2l + k\log(npar)} where *npar*
+#'  represents the number of parameters in the fitted model
+#'  and \eqn{l} represents model log-likelihood
+#'
+#' @import stats
+#' @export
+#'
+BIC.light.zeroinfl <- function(object, ...){
+  k <- nrow(object$coefficients$count) + nrow(object$coefficients$zero)
+  return(
+    -2*object$loglik + k*log(object$n)
+  )
+}
+
+
+#' @export
+#'
+BIC.light.glm <- function(object, ...){
+
+  llk <- as.numeric(logLik(object))
+  k <- nrow(object$coefficients)
+
+  if (!is.null(object$theta)) k <- k + 1
+
+  return(
+    -llk + k*log(object$n)
+  )
+
+}
