88bc219d0ccf294b83bd2a5ae23c4aa730191c71
Lino Galiana <linogaliana@yahoo.fr>
2020-03-30 22:39:15 +0200

    test extract_coeff routine

---
+++ tests/testthat/test-extract-coef.R
@@ -0,0 +1,53 @@
+testthat::context("extract_coeff recovers the coefficients")
+
+
+library(texlight)
+
+# GLM
+
+
+# LIGHT.GLM
+
+glm <- glm(
+  I(round(Sepal.Length)) ~ Sepal.Width,
+  data = iris,
+  family = poisson()
+)
+
+light_glm <- texlight::strip(glm)
+
+stats_glm <- texlight::extract_coeff(light_glm)
+
+testthat::test_that("Coefficient names are consistent", {
+  testthat::expect_equal(
+    stats_glm[,'variable'],
+    names(glm$coefficients)
+  )
+})
+
+output_glm <- summary(glm)$coefficients
+
+expected_coeff <- sapply(seq_len(nrow(output_glm)), function(i){
+  paste0(format(output_glm[i, 'Estimate'], nsmall = 3L,
+                digits = 2L),
+         signif_stars(output_glm[i,'Pr(>|z|)']))
+})
+
+testthat::test_that("Coefficient values are consistent", {
+  testthat::expect_equal(
+    stats_glm[,'text_coeff'],
+    expected_coeff
+  )
+})
+
+
+testthat::test_that("Coefficient s.e. are consistent", {
+  testthat::expect_equal(
+    stats_glm[,'text_sd'],
+    paste0("(",
+           format(output_glm[,'Std. Error'],
+                  digits = 2L, nsmall = 3L
+                  ),
+           ")")
+  )
+})
