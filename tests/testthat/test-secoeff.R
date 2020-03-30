82d5f2f0153a2f62896d43db3bf22d75384c648d
Lino Galiana <linogaliana@yahoo.fr>
2020-03-30 21:56:52 +0200

    More tests for secoeff

--- tests/testthat/test-secoeff.R
+++ tests/testthat/test-secoeff.R
@@ -1,6 +1,7 @@
 testthat::context("se_coeff methods return coefficients s.e., t stat and pvalues")
 
 
+# 1. OLS -------------------
 
 ols <- lm(
   Sepal.Length ~ Sepal.Width,
@@ -13,8 +14,12 @@ testthat::expect_equal(
   summary(ols)$coefficients
 )
 
+testthat::expect_equal(
+  secoeff(strip(ols)),
+  summary(ols)$coefficients
+)
 
-
+# 2. OGLM -------------------
 
 iris$y_r <- as.numeric(iris$Species)
 
@@ -29,4 +34,32 @@ testthat::expect_equal(
   summary(oglm)$estimate
 )
 
+
+# 3. NEGBIN ----------
+
+# quine.nb1 <- MASS::glm.nb(Days ~ Sex/(Age + Eth*Lrn), data = MASS::quine)
+
+
+# testthat::expect_equal(
+#   texlight::secoeff(quine.nb1),
+#   summary(quine.nb1)$estimate
+# )
+
+
+# 4. secoeff.light.zeroinfl
+
+data("bioChemists", package = "pscl")
+
+zeroinfl_negbin <- pscl::zeroinfl(art ~ . | ., data = bioChemists, dist = "negbin")
+zeroinfl_negbin_strip <- texlight::strip(zeroinfl_negbin)
+
+testthat::expect_equal(
+  secoeff(zeroinfl_negbin_strip, modeltype = "count"),
+  secoeff(zeroinfl_negbin)$count
+)
+
+testthat::expect_equal(
+  secoeff(zeroinfl_negbin_strip, modeltype = "selection"),
+  secoeff(zeroinfl_negbin)$zero
+)
 
