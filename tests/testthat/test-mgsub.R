d1cdd2d9db471879c9e9dd65c764e76bd80fae5d
Lino Galiana <linogaliana@yahoo.fr>
2020-04-06 22:11:24 +0200

    requirenamespace mgsub

--- tests/testthat/test-mgsub.R
+++ tests/testthat/test-mgsub.R
@@ -1,5 +1,6 @@
 testthat::context("mgsub: vectorized multigsub")
 
+requireNamespace("mgsub", quietly = TRUE)
 
 testthat::expect_equal(
   mgsub(c("it's", "I'm"), c("it is", "I am"), c("I'm the king of the worlds, it's so funny","it's impossible!")),
