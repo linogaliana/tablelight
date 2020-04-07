2ac2373d4492e43e8d4ce631ad24ed97c4050eda
Lino Galiana <linogaliana@yahoo.fr>
2020-04-07 21:22:44 +0200

    doc view_html

--- R/view_html.R
+++ R/view_html.R
@@ -1,3 +1,9 @@
+#' Report HTML table in Rstudio viewer
+#'
+#'  @param x HTML code
+#'  @return Returns nothing, HTML reported in
+#'   Rstudio viewer
+#'  @importFrom rstudioapi viewer
 view_html <- function(x){
   tab <- paste(x, collapse = "")
   tf <- tempfile(fileext = ".html")
