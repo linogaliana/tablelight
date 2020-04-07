174a88f0e9aa9b93be8a7c2c12f96ec961f03436
Lino Galiana <linogaliana@yahoo.fr>
2020-04-07 21:59:08 +0200

    test signif_stars

--- tests/testthat/test-signif_stars.R
+++ tests/testthat/test-signif_stars.R
@@ -14,3 +14,26 @@ testthat::test_that(
                            "")
   }
 )
+
+
+testthat::test_that(
+  "Number of stars match with expectation", {
+    testthat::expect_equal(signif_stars(NA, type = "html"),
+                           "")
+    testthat::expect_equal(signif_stars(0.005, type = "html"),
+                           "<sup>***</sup>")
+    testthat::expect_equal(signif_stars(0.02, type = "html"),
+                           "<sup>**</sup>")
+    testthat::expect_equal(signif_stars(0.07, type = "html"),
+                           "<sup>*</sup>")
+    testthat::expect_equal(signif_stars(0.15, type = "html"),
+                           "")
+  }
+)
+
+
+testthat::test_that("Only accepted answers are latex or html",{
+  testthat::expect_error(signif_stars(0.02, type = "HTML"))
+  testthat::expect_error(signif_stars(0.02, type = "LatEx"))
+  testthat::expect_error(signif_stars(0.02, type = "anything"))
+})
