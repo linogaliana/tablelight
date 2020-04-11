7f7e0a4c903581199286155bb7e68ae32bf1266d
Lino Galiana <linogaliana@yahoo.fr>
2020-04-11 12:19:36 +0200

    Add reference_level_position tests

--- tests/testthat/test-light_table_nnet.R
+++ tests/testthat/test-light_table_nnet.R
@@ -1,3 +1,5 @@
+# WITHOUT reference_level_position
+
 n<-250
 x1<-sample(c(0,1),n,replace=TRUE,prob=c(0.75,0.25))
 x2<-vector("numeric",n)
@@ -19,7 +21,7 @@ logit <- nnet::multinom(y ~ x1 + x2 + z, data=dataset)
 
 
 latex_table <- light_table(
-    logit, type = "latex", stats.list = c("n","ll","lln","bic","link")
+  logit, type = "latex", stats.list = c("n","ll","lln","bic","link")
 )
 
 
@@ -29,7 +31,7 @@ testthat::test_that("Count and Selection distributions: empty over 3 columns", {
   testthat::expect_equal(
     sum(
       grepl("Count distribution & \\\\multicolumn\\{3\\}\\{c\\}\\{\\} \\\\\\\\", latex_table,
-          perl = TRUE)
+            perl = TRUE)
     ),
     1L
   )
@@ -54,7 +56,7 @@ testthat::test_that("Log-likelihood, Log-likelihood by obs and BIC over 3 column
         sprintf("Log likelihood & \\\\multicolumn\\{3\\}\\{c\\}\\{\\$-\\$%s\\} \\\\\\\\",
                 abs(as.numeric(round(logLik(logit))))),
         latex_table)
-      ),
+    ),
     1L
   )
 
@@ -64,7 +66,7 @@ testthat::test_that("Log-likelihood, Log-likelihood by obs and BIC over 3 column
         sprintf("Log likelihood \\(by obs\\.\\) & \\\\multicolumn\\{3\\}\\{c\\}\\{\\$-\\$%s\\} \\\\\\\\",
                 format(
                   abs(as.numeric(logLik(logit)))/nrow(logit$residuals),
-                digits = 3L, nsmall = 3L)
+                  digits = 3L, nsmall = 3L)
         ),
         latex_table)
     ),
@@ -81,5 +83,103 @@ testthat::test_that("Log-likelihood, Log-likelihood by obs and BIC over 3 column
     1L
   )
 
+
+})
+
+
+# With reference_level_position
+
+
+latex_table <- light_table(
+  logit, type = "latex", stats.list = c("n","ll","lln","bic","link"),
+  reference_level_position = 2L
+)
+
+
+testthat::test_that("Count and Selection distributions: empty over 4 columns", {
+
+  testthat::expect_equal(
+    sum(
+      grepl("Count distribution & \\\\multicolumn\\{4\\}\\{c\\}\\{\\} \\\\\\\\", latex_table,
+            perl = TRUE)
+    ),
+    1L
+  )
+
+  testthat::expect_equal(
+    sum(
+      grepl("Selection distribution & \\\\multicolumn\\{4\\}\\{c\\}\\{\\} \\\\\\\\", latex_table,
+            perl = TRUE)
+    ),
+    1L
+  )
+
+
+})
+
+
+testthat::test_that("Log-likelihood, Log-likelihood by obs and BIC over 4 columns", {
+
+  testthat::expect_equal(
+    sum(
+      grepl(
+        sprintf("Log likelihood & \\\\multicolumn\\{4\\}\\{c\\}\\{\\$-\\$%s\\} \\\\\\\\",
+                abs(as.numeric(round(logLik(logit))))),
+        latex_table)
+    ),
+    1L
+  )
+
+  testthat::expect_equal(
+    sum(
+      grepl(
+        sprintf("Log likelihood \\(by obs\\.\\) & \\\\multicolumn\\{4\\}\\{c\\}\\{\\$-\\$%s\\} \\\\\\\\",
+                format(
+                  abs(as.numeric(logLik(logit)))/nrow(logit$residuals),
+                  digits = 3L, nsmall = 3L)
+        ),
+        latex_table)
+    ),
+    1L
+  )
+
+  testthat::expect_equal(
+    sum(
+      grepl(
+        sprintf("Bayesian information criterion & \\\\multicolumn\\{4\\}\\{c\\}\\{%s\\} \\\\\\\\",
+                round(BIC(logit))),
+        latex_table)
+    ),
+    1L
+  )
+
+
+})
+
+
+
+
+latex_table_noref <- light_table(
+  logit, type = "latex", stats.list = c("n","ll","lln","bic","link")
+)
+
+body_part_noref <- latex_table_noref[grep("(Intercept)", latex_table_noref):(grep("^z", latex_table_noref) + 2)]
+body_part_ref <- latex_table[grep("(Intercept)", latex_table_noref):(grep("^z", latex_table_noref) + 2)]
+
+
+body_part_noref <- stringr::str_split(body_part_noref, "&", simplify = TRUE)
+body_part_ref   <- stringr::str_split(body_part_ref, "&", simplify = TRUE)
+
+
+testthat::test_that("Same table except for reference column", {
+
+  testthat::expect_equal(
+    cbind(
+      trimws(body_part_noref[,1:2]),
+      trimws(rep(c("(Ref) ", " ", " "), 4L)),
+      trimws(body_part_noref[,3:4])
+    ),
+    trimws(body_part_ref)
+  )
 
 })
