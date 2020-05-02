6463e4a8920f797d9bd99cd8c48d748f74455313
Lino Galiana <linogaliana@yahoo.fr>
2020-05-02 17:44:53 +0200

    create print_html function
    Necessary to use htmltools for the moment

--- R/view_html.R
+++ R/view_html.R
@@ -1,8 +1,9 @@
-#' Report HTML table in Rstudio viewer
+#' Report HTML table in Rstudio viewer or in a Rmarkdown document
 #'
 #' @param x HTML code
 #' @return Returns nothing, HTML reported in
-#'   Rstudio viewer
+#'   Rstudio viewer (`view_html`) or in
+#'   the Rmd document (`print_html`)
 #' @importFrom rstudioapi viewer
 #' @export
 view_html <- function(x){
@@ -11,3 +12,14 @@ view_html <- function(x){
   writeLines(tab, tf)
   rstudioapi::viewer(tf)
 }
+
+#' @rdname view_html
+#' @export
+#' @importFrom htmltools includeHTML
+print_html <- function(x){
+  tab <- paste(x, collapse = "")
+  tf <- tempfile(fileext = ".html")
+  writeLines(tab, tf)
+  htmltools::includeHTML(tf)
+}
+
