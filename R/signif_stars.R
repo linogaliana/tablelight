4c684735ae1e5cd8915439fdd12800d496eb41d1
Lino Galiana <lino.galiana@insee.fr>
2020-06-16 13:51:24 +0200

    Add vectorized significant stars function

--- R/signif_stars.R
+++ R/signif_stars.R
@@ -4,16 +4,38 @@
 #' @param type Should the stars be *latex*
 #'   or *html* formatted ?
 
-signif_stars <- function(pvalue, type = c("latex", "html")){
+signif_stars <- function(pvalue, type = c("latex", "html", "none")){
 
   type <- match.arg(type)
 
-  wildcard1 <- ifelse(identical(type, "latex"), "$^{", "<sup>")
-  wildcard2 <- ifelse(identical(type, "latex"), "}$", "</sup>")
+  wildcard1 <- switch(
+    type,
+    latex = "$^{",
+    html = "<sup>",
+    none = ""
+  )
+
+  wildcard2 <- switch(
+    type,
+    latex = "}$",
+    html = "</sup>",
+    none = ""
+  )
 
   if (is.na(pvalue)) return("")
   if (pvalue<0.01) return(sprintf("%s***%s", wildcard1, wildcard2))
   if (pvalue<0.05) return(sprintf("%s**%s", wildcard1, wildcard2))
   if (pvalue<0.1) return(sprintf("%s*%s", wildcard1, wildcard2))
   return("")
+}
+
+
+signif_stars_vectorized <- function(pvalue, type = c("latex", "html", "none")){
+
+  type <- match.arg(type)
+
+  return(
+    sapply(pvalue, signif_stars, type = type)
+  )
+
 }
--- tests/testthat/test-signif_stars.R
+++ tests/testthat/test-signif_stars.R
@@ -32,8 +32,54 @@ testthat::test_that(
 )
 
 
-testthat::test_that("Only accepted answers are latex or html",{
+testthat::test_that(
+  "Number of stars match with expectation", {
+    testthat::expect_equal(signif_stars(NA, type = "none"),
+                           "")
+    testthat::expect_equal(signif_stars(0.005, type = "none"),
+                           "***")
+    testthat::expect_equal(signif_stars(0.02, type = "none"),
+                           "**")
+    testthat::expect_equal(signif_stars(0.07, type = "none"),
+                           "*")
+    testthat::expect_equal(signif_stars(0.15, type = "none"),
+                           "")
+  }
+)
+
+
+testthat::test_that("Only accepted answers are 'latex', 'html' or 'none'",{
   testthat::expect_error(signif_stars(0.02, type = "HTML"))
   testthat::expect_error(signif_stars(0.02, type = "LatEx"))
+  testthat::expect_error(signif_stars(0.02, type = "None"))
   testthat::expect_error(signif_stars(0.02, type = "anything"))
 })
+
+
+# vectorized -------------------
+
+testthat::expect_identical(
+  signif_stars_vectorized(
+    c(NA, 0.005, 0.02, 0.07, 0.15),
+    type = "none"
+  ),
+  c("","***","**","*","")
+)
+
+testthat::expect_identical(
+  signif_stars_vectorized(
+    c(NA, 0.005, 0.02, 0.07, 0.15),
+    type = "html"
+  ),
+  c("","<sup>***</sup>","<sup>**</sup>",
+    "<sup>*</sup>","")
+)
+
+testthat::expect_identical(
+  signif_stars_vectorized(
+    c(NA, 0.005, 0.02, 0.07, 0.15),
+    type = "latex"
+  ),
+  c("","$^{***}$","$^{**}$",
+    "$^{*}$","")
+)
