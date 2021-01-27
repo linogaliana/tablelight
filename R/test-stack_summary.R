3441f9be9f4acbd802f7a6f8e3a251f4eff5c184
Lino Galiana <linogaliana@yahoo.fr>
2021-01-27 13:29:06 +0100

    failing test added
    cf. #79

---
+++ R/test-stack_summary.R
@@ -0,0 +1,16 @@
+testthat::context("stack_summary makes nice summary stats tables")
+
+df <- data.frame(
+  x = rnorm(10),
+  y = 1:10
+)
+
+tablelight:::stack_summary(df, x_vars = c("x"))
+tablelight:::stack_summary(list(df, df), x_vars = c("x","y"))
+
+testthat::expect_equal(
+  tablelight:::stack_summary(df, x_vars = c("x"),
+                           stats = c("1Q","mean",'median','3Q','P90','N')),
+  tablelight:::stack_summary(df, x_vars = c("x"),
+                           stats = c("mean","1Q",'median','3Q','P90','N'))
+)
